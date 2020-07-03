53ce60ba4da67e3a3bb213f687c86eb45e80f65d
anardy <anardy@amiltone.com>
2020-07-03 20:23:31 +0200

    Correcting mysql comamnd

--- galette/install/scripts/upgrade-to-0.932-mysql.sql
+++ galette/install/scripts/upgrade-to-0.932-mysql.sql
@@ -3,10 +3,9 @@ SET FOREIGN_KEY_CHECKS=0;
 ALTER TABLE galette_adherents
 DROP COLUMN fingerprint;
 
-ALTER TABLE galette_adherents
-RENAME COLUMN icq_adh TO twitter_adh, 
-RENAME COLUMN msn_adh TO discord_adh, 
-RENAME COLUMN jabber_adh TO telegram_adh;
+ALTER TABLE galette_adherents CHANGE `icq_adh` TO `twitter_adh` VARCHAR(20);
+ALTER TABLE galette_adherents CHANGE `msn_adh` TO `discord_adh` VARCHAR(150);
+ALTER TABLE galette_adherents CHANGE `jabber_adh` TO `telegram_adh` VARCHAR(150);
 
 UPDATE galette_database SET version = 0.932;
 SET FOREIGN_KEY_CHECKS=1;
